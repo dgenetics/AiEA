da5746179fa3adaaa4e2349e879eaf3d559d80ce
dgenetics <will.djinis@gmail.com>
2026-07-13 23:30:31 -0400

    Disable pinch and double-tap zoom in the app viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export const viewport: Viewport = {
   themeColor: "#07070b",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
